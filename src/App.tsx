0a593008fcdd8ddc397ddef8b28f0c4eb8fb2c98
bipsBro <bpnpanday313@gmail.com>
2019-07-04 19:28:45 +0545

    config eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import React from "react";
 let x;
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <div>sdf</div>React - typescript - boilerplate
-    </div>
-  );
+  return <div>React - typescript - boilerplate</div>;
 };
 
 export default App;
